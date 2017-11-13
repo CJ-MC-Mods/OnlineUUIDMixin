73ae4ae3760b84c56d8e1c1bba1177df0c3a5494
CJ Miller <cjmiller721@gmail.com>
2017-11-13 01:19:46 -0800

    Trying more changes

--- src/main/java/com/cjm721/onlineuuid/OnlineUUIDMixin.java
+++ src/main/java/com/cjm721/onlineuuid/OnlineUUIDMixin.java
@@ -1,18 +1,22 @@
 package com.cjm721.onlineuuid;
 
 
+import com.mojang.authlib.GameProfile;
 import net.minecraft.network.login.INetHandlerLoginServer;
 import net.minecraft.server.network.NetHandlerLoginServer;
 import net.minecraft.util.ITickable;
+import net.minecraftforge.fml.server.FMLServerHandler;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+import java.util.UUID;
 
 @Mixin(NetHandlerLoginServer.class)
 public abstract class OnlineUUIDMixin implements INetHandlerLoginServer, ITickable {
-
-    //@Redirect(method = "getOfflineProfile", at = @At("HEAD"))
-//    protected GameProfile getOfflineProfile(GameProfile original)
-//    {
-//        UUID uuid = FMLServerHandler.instance().getServer().getPlayerProfileCache().getGameProfileForUsername(original.getName()).getId();
-//        return new GameProfile(uuid, original.getName());
-//    }
+    @Overwrite
+    protected GameProfile getOfflineProfile(GameProfile original)
+    {
+        UUID uuid = FMLServerHandler.instance().getServer().getPlayerProfileCache().getGameProfileForUsername(original.getName()).getId();
+        return new GameProfile(uuid, original.getName());
+    }
 }
